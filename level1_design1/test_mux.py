9bdb95dcec82ac5341cc3d542b52b8673065b4b2
Navinkumar K <knavin2002@gmail.com>
2022-07-20 17:51:34 +0000

    test

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -5,8 +5,15 @@ from cocotb.triggers import Timer
 
 @cocotb.test()
 async def test_mux(dut):
-    """Test for mux2"""
+    """Test for mux2 where we test a single input"""
 
     cocotb.log.info('##### CTB: Develop your test here ########')
+
+    input_port = str(dut.inp)+str(dut.sel.value)
+
+    dut.inp12.value = 1;
+
+    assert dut.out.value == dut.inp12.value "MUX failed with input = {inp12}, select = {SEL}".format(
+            inp12=dut.inp12.value, SEL=dut.sel.value)
 
     

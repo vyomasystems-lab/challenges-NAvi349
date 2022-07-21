d291dabac3a495a9492a2798628f67a5e1ebbcf5
Navinkumar K <knavin2002@gmail.com>
2022-07-21 17:40:07 +0000

    testbench

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -10,6 +10,25 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge
 
+
+def subseq(bit, input_seq):
+
+    """
+    Finds the 1011 sub-sequence in the input sequence.
+    If found it will until the last before bit
+    Suppose 1010110 is detected, then the sequence now is 10
+    This is help us to detect overlapping 1011 patterns
+    """
+    #print(input_seq)    
+
+    if ('1011' in input_seq):
+        pos = input_seq.find('1011')
+        input_seq = input_seq[(pos+3):]
+        #print("new str = " + input_seq)
+        return [1, input_seq]
+    else:
+        return 0
+
 @cocotb.test()
 async def test_seq_bug1(dut):
     """Test for seq detection """
@@ -24,5 +43,21 @@ async def test_seq_bug1(dut):
     await FallingEdge(dut.clk)
 
     cocotb.log.info('#### CTB: Develop your test here! ######')
+    input_seq = str()
 
-    
+    for times in range(0, 10):
+        bit = random.randint(0, 1)
+
+
+        
+        input_seq = input_seq + str(bit)
+
+        [detect_out, input_seq] = subseq(bit, input_seq)
+
+        dut.inp_bit.value = bit
+
+        await FallingEdge(dut.clk)
+
+        dut._log.info(f'inp_bit = {dut.inp_bit.value} expected_output={detect_out} DUT = {dut.seq_seen.value}')
+
+

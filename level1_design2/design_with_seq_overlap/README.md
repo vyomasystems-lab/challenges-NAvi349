b9f1d1d62dabb057bb233f6f9b2e98c2b51a3a41
Navinkumar K <knavin2002@gmail.com>
2022-07-22 14:37:25 +0000

    duplicated optional

---
+++ level1_design2/design_with_seq_overlap/README.md
@@ -0,0 +1,8 @@
+#This folder contains the design which will work for sequence overlap
+
+![image](https://user-images.githubusercontent.com/66086031/180295189-930832ff-3515-42e5-ac15-293d220d49b3.png)
+![image](https://user-images.githubusercontent.com/66086031/180295250-f755067b-1af8-4f6b-8966-f58b0546a8c2.png)
+![image](https://user-images.githubusercontent.com/66086031/180295434-3f43ca5b-75a9-4fd1-b9e1-6a03751be15b.png)
+![image](https://user-images.githubusercontent.com/66086031/180295545-e4fa6348-67bb-40e3-88ea-58f619b23030.png)
+
+
---
+++ level1_design2/design_with_seq_overlap/test_seq_detect_1011.py
@@ -0,0 +1,76 @@
+# See LICENSE.vyoma for details
+
+# SPDX-License-Identifier: CC0-1.0
+
+import os
+import random
+from pathlib import Path
+
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, FallingEdge
+
+
+def subseq(bit, input_seq):
+
+    """
+    Detects overlapping sequences
+    Finds the 1011 sub-sequence in the input sequence.
+    If found it will until the last before bit
+    Suppose 1010110 is detected, then the sequence now is 10
+    This is help us to detect overlapping 1011 patterns
+    """
+    #print(input_seq)    
+
+    if ('1011' in input_seq):
+        return 1
+    else:
+        return 0
+
+def format_seq(input_seq):
+
+    if ('1011' in input_seq):
+        pos = input_seq.find('1011')
+        input_seq = input_seq[(pos+3):]  #delete till the last but one. So that it will become part of the next sequence
+            
+    return input_seq
+    
+
+@cocotb.test()
+async def test_seq_bug1(dut):
+    """Test for seq detection """
+
+    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
+    cocotb.start_soon(clock.start())        # Start the clock
+
+    # reset
+    dut.reset.value = 1
+    await FallingEdge(dut.clk)  
+    dut.reset.value = 0
+    await FallingEdge(dut.clk)
+
+    cocotb.log.info('#### CTB: Develop your test here! ######')
+    input_seq = str() #store the input sequence
+    org_seq = str() #copy of the original sequence
+
+    for times in range(0, 40):  #generate 40 input bits
+        bit = random.randint(0, 1)
+        org_seq = org_seq + str(bit)
+        
+        input_seq = input_seq + str(bit)
+        
+
+        detect_out = subseq(bit, input_seq)    #detect output bit using function
+        input_seq = format_seq(input_seq) #remove the valid sequence from the input sequence after detection
+
+        dut.inp_bit.value = bit
+
+        await FallingEdge(dut.clk)
+
+        dut._log.info(f'inp_bit = {dut.inp_bit.value} expected_output = {detect_out} DUT = {dut.seq_seen.value}')
+        
+
+        assert detect_out==dut.seq_seen.value, "FSM failed for the input sequence = {seq}".format(seq = org_seq)
+
+            
+

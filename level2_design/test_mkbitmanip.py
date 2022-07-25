010458a9b786a94ac8a4e15a867632a7358067c0
Navinkumar K <knavin2002@gmail.com>
2022-07-25 15:46:11 +0000

    ok

--- level2_design/test_mkbitmanip.py
+++ level2_design/test_mkbitmanip.py
@@ -38,7 +38,8 @@ def run_test(dut):
         mav_putvalue_src1 = 0x5
         mav_putvalue_src2 = 0x0
         mav_putvalue_src3 = 0x0
-        mav_putvalue_instr = 0x101010B3
+        //mav_putvalue_instr = 0x101010B3
+        
 
         # expected output from the model
         expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)

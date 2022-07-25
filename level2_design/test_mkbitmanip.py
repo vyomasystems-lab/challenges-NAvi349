9e817695df6ce0d437067808ccb5667ccfaa0e14
Navinkumar K <knavin2002@gmail.com>
2022-07-25 18:53:42 +0000

    test bugged

--- level2_design/test_mkbitmanip.py
+++ level2_design/test_mkbitmanip.py
@@ -34,33 +34,55 @@ def run_test(dut):
 
     ######### CTB : Modify the test to expose the bug #############
     # input transaction
+
+    # opening the file in read mode
+    my_file = open("imem.txt", "r")
     
-    mav_putvalue_src1 = 0x48855885
-    mav_putvalue_src2 = 0x1099
-    mav_putvalue_src3 = 0x00
-    mav_putvalue_instr = 0x605353B3
-
-
-    # expected output from the model
-    expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)
-
-    # driving the input transaction
-    dut.mav_putvalue_src1.value = mav_putvalue_src1
-    dut.mav_putvalue_src2.value = mav_putvalue_src2
-    dut.mav_putvalue_src3.value = mav_putvalue_src3
-    dut.EN_mav_putvalue.value = 1
-    dut.mav_putvalue_instr.value = mav_putvalue_instr
-
-    yield Timer(1) 
-
-    # obtaining the output
-    dut_output = dut.mav_putvalue.value
-
-    cocotb.log.info(f'DUT OUTPUT={hex(dut_output)}')
-    cocotb.log.info(f'EXPECTED OUTPUT={hex(expected_mav_putvalue)}')
+    # reading the file
+    data = my_file.read()
     
-    # comparison
-    error_message = f'Value mismatch DUT = {hex(dut_output)} does not match MODEL = {hex(expected_mav_putvalue)}'
-    assert dut_output == expected_mav_putvalue, error_message
+    # replacing end splitting the text 
+    # when newline ('\n') is seen.
+    mem = data.split("\n")
+    mem = [int("0x"+instr, 16) for instr in mem]
+
+    my_file.close()  
+      
+
+    for inst in mem:
+        mav_putvalue_src1 = random.randint(-10, 10)
+        mav_putvalue_src2 = random.randint(-10, 10)
+        mav_putvalue_src3 = random.randint(-10, 10)
+
         
-            
+        mav_putvalue_instr = inst
+
+
+        # expected output from the model
+        expected_mav_putvalue = bitmanip(mav_putvalue_instr, mav_putvalue_src1, mav_putvalue_src2, mav_putvalue_src3)
+
+        # driving the input transaction
+        dut.mav_putvalue_src1.value = mav_putvalue_src1
+        dut.mav_putvalue_src2.value = mav_putvalue_src2
+        dut.mav_putvalue_src3.value = mav_putvalue_src3
+        dut.EN_mav_putvalue.value = 1
+        dut.mav_putvalue_instr.value = mav_putvalue_instr
+
+        yield Timer(1) 
+
+        # obtaining the output
+        dut_output = dut.mav_putvalue.value
+
+
+        
+        # comparison
+        error_message = f'Value mismatch DUT = {hex(dut_output)} does not match MODEL = {hex(expected_mav_putvalue)}'
+        try :
+            assert dut_output == expected_mav_putvalue
+        except AssertionError:
+            cocotb.log.info(f'INSTR={hex(inst)}')
+            cocotb.log.info(f'SRC1={hex(mav_putvalue_src1)} SRC2={hex(mav_putvalue_src2)} SRC3={hex(mav_putvalue_src3)}')
+            cocotb.log.info(f'DUT OUTPUT={hex(dut_output)}')
+            cocotb.log.info(f'EXPECTED OUTPUT={hex(expected_mav_putvalue)}')
+            print(error_message)
+

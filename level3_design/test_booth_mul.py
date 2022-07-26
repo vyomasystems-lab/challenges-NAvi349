95b61a0f53f5d57a7c3e587a186bd6ddc10613b3
Navinkumar K <knavin2002@gmail.com>
2022-07-26 09:15:25 +0000

    finalizng tb

--- level3_design/test_booth_mul.py
+++ level3_design/test_booth_mul.py
@@ -5,57 +5,48 @@ from pathlib import Path
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge
-
+from cocotb.triggers import Timer
 
 
 @cocotb.test()
-async def test_seq_bug1(dut):
+async def test_seq_1(dut):
     """Test for booth radix-2 algorithm"""
+
+    Multiplicand = random.randint(0, 2**4) 
+    Multiplier = random.randint(0, 2**3)
+    Result = Multiplicand * Multiplier
+
+    await Timer(10, units="ns")
 
     clock = Clock(dut.clk, 10, units="ns")  # Create a 10us period clock on port clk
     cocotb.start_soon(clock.start())        # Start the clock
 
-    # reset will be provided by the first state of the DUT
-   
+    # reset will be provided by the first state of the DUT 
+    
+
+    await RisingEdge(dut.clk)
 
     cocotb.log.info('#### CTB: Test Starts ######')
-    cocotb.log.info('#### Initializing Data Input ######')
+    #cocotb.log.info('#### Initializing Data Input ######')
     dut.data_in.value = 0
     dut.start.value = 0
     
     await RisingEdge(dut.clk)   # wait for next clock edge after giving input
     cocotb.log.info('#### Initializing Start State ######')
-    cocotb.log.info('start = {dut.start.value}')
+    cocotb.log.info(f'start = {dut.start.value}')
     dut.start.value = 1
 
     await RisingEdge(dut.clk)
-    cocotb.log.info('#### Multiplicand ######')
+    cocotb.log.info('#### Loading Multiplicand ######')
     cocotb.log.info(f'start = {dut.start.value}')
-
-    dut.data_in.value = 0xFFF6  # -10
+    dut.data_in.value = Multiplicand
 
     await RisingEdge(dut.clk)
-    cocotb.log.info('#### Multiplier ######')
-    
-    dut.data_in.value = 0xD  # 13    
-
-    await RisingEdge(dut.clk)    
-    await RisingEdge(dut.clk)    
-    await RisingEdge(dut.clk)    
-    await RisingEdge(dut.clk)
-    await RisingEdge(dut.clk)  
-    await RisingEdge(dut.clk)    
-    await RisingEdge(dut.clk)  
-    await RisingEdge(dut.clk)
-    await RisingEdge(dut.clk)  
-    await RisingEdge(dut.clk)  
-    await RisingEdge(dut.clk)  
-    
-    #130ns
+    cocotb.log.info('#### Loading Multiplier ######')    
+    dut.data_in.value = Multiplier 
  
-    dut._log.info(f'DUT = {dut.done.value}')
+        
+    await Timer(100, units="ns")
+    dut._log.info(f'DONE = {dut.done.value}')
 
-
-    #dut._log.info(f'expected_output = {detect_out} DUT = {dut.seq_seen.value}')        
-
-    #assert detect_out==dut.seq_seen.value, "FSM failed for the input sequence = {seq}".format(seq = org_seq)
+    dut._log.info(f"Expected value = {Result} DUT value {int(hex(dut.out.value), 16)}")

6aa1304f4272793ea216780d274bebaa43bc711a
Navinkumar K <knavin2002@gmail.com>
2022-07-26 09:48:54 +0000

    final tb

--- level3_design/test_booth_mul.py
+++ level3_design/test_booth_mul.py
@@ -6,26 +6,29 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge
 from cocotb.triggers import Timer
+import cocotb.binary
 
 
 @cocotb.test()
 async def test_seq_1(dut):
-    """Test for booth radix-2 algorithm"""
-
-    Multiplicand = random.randint(0, 2**4) 
-    Multiplier = random.randint(0, 2**3)
-    Result = Multiplicand * Multiplier
-
-    await Timer(10, units="ns")
+    """
+    Test for booth radix-2 algorithm. Inputs are 4 bits. 
+    So the possible range of values is -16 to 15.
+    """
 
     clock = Clock(dut.clk, 10, units="ns")  # Create a 10us period clock on port clk
     cocotb.start_soon(clock.start())        # Start the clock
 
+
+    Multiplicand = random.randint(-2**4, (2**4)-1) 
+    Multiplier = random.randint(-2**4, (2**4)-1)
+    Result = Multiplicand * Multiplier
+
+    await Timer(10, units="ns")
     # reset will be provided by the first state of the DUT 
     
 
     await RisingEdge(dut.clk)
-
     cocotb.log.info('#### CTB: Test Starts ######')
     #cocotb.log.info('#### Initializing Data Input ######')
     dut.data_in.value = 0
@@ -46,7 +49,8 @@ async def test_seq_1(dut):
     dut.data_in.value = Multiplier 
  
         
-    await Timer(100, units="ns")
+    await Timer(120, units="ns")
     dut._log.info(f'DONE = {dut.done.value}')
 
-    dut._log.info(f"Expected value = {Result} DUT value {int(hex(dut.out.value), 16)}")
+    dut._log.info(f"A = {Multiplicand} B = {Multiplier}") 
+    dut._log.info(f"Expected value = {Result} DUT value {dut.out.value.signed_integer}")
